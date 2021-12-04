49a07f30f7886a905580949a35473276761aacb8
FRED LE COQUIL <flecoqui@microsoft.com>
2021-12-04 22:20:03 +0000

    End-to-end test successful

--- scripts/azure-test.sh
+++ scripts/azure-test.sh
@@ -10,7 +10,8 @@ parent_path=$(
     cd "$(dirname "${BASH_SOURCE[0]}")/../"
     pwd -P
 )
-source `dirname $0`/common.sh
+SCRIPTS_DIRECTORY=`dirname $0`
+source "$SCRIPTS_DIRECTORY"/common.sh
 
 # container version (current date)
 export APP_VERSION=$(date +"%y%M%d.%H%M%S")
@@ -57,25 +58,49 @@ function deployAzureInfrastructure(){
         --name $datadep \
         --resource-group $resourcegroup \
         --subscription $subscription \
-        --template-file `dirname $0`/arm-template.json \
-        --output non \
+        --template-file $SCRIPTS_DIRECTORY/arm-template.json \
+        --output none \
         --parameters \
         webAppName=$webapp sku=$sku"
     printProgress "$cmd"
     eval "$cmd"
     checkError
+    
     # get ACR login server dns name
-    ARC_LOGIN_SERVER=$(az deployment group show --resource-group $resourcegroup -n $datadep | jq -r '.properties.outputs.acrLoginServer.value')
+    ACR_LOGIN_SERVER=$(az deployment group show --resource-group $resourcegroup -n $datadep | jq -r '.properties.outputs.acrLoginServer.value')
     # get WebApp Url
     WEB_APP_SERVER=$(az deployment group show --resource-group $resourcegroup -n $datadep | jq -r '.properties.outputs.webAppServer.value')
+    # get ACR Name
+    ACR_NAME=$(az deployment group show --resource-group $resourcegroup -n $datadep | jq -r '.properties.outputs.acrName.value')
+    
+    cmd="az acr update -n $ACR_NAME --admin-enabled true --output none"
+    printProgress "$cmd"
+    eval "$cmd"
+    checkError
+
+    # get ACR password
+    ACR_PASSWORD=$(az acr credential show -n "$ACR_NAME" --query passwords[0].value --output tsv)
+    # get ACR login
+    ACR_LOGIN=$(az acr credential show -n "$ACR_NAME" --query username --output tsv)
+}
+
+function undeployAzureInfrastructure(){
+    subscription=$1
+    prefix=$2
+    resourcegroup="${prefix}rg"
+
+    cmd="az group delete  --subscription $subscription  --name $resourcegroup --output none "
+    printProgress "$cmd"
+    eval "$cmd"
 }
 
 function buildWebAppContainer() {
-    apiModule="$1"
-    imageName="$2"
-    imageTag="$3"
-    imageLatestTag="$4"
-    portHttp="$5"
+    ContainerRegistryName="$1"
+    apiModule="$2"
+    imageName="$3"
+    imageTag="$4"
+    imageLatestTag="$5"
+    portHttp="$6"
 
     targetDirectory="$(dirname "${BASH_SOURCE[0]}")/../$apiModule"
 
@@ -91,29 +116,49 @@ function buildWebAppContainer() {
 
     # Build the image
     echo "Building the docker image for '$imageName:$imageTag'"
-    docker build -t ${imageName}:${imageTag} -f Dockerfile --build-arg APP_VERSION=${imageTag} --build-arg ARG_PORT_HTTP=${portHttp} .
-    # Push with alternative tag
-    docker tag ${imageName}:${imageTag} ${imageName}:${imageLatestTag}
+    cmd="az acr build --registry $ContainerRegistryName --image ${imageName}:${imageTag} --image ${imageName}:${imageLatestTag} -f Dockerfile --build-arg APP_VERSION=${imageTag} --build-arg ARG_PORT_HTTP=${portHttp} . --output none"
+    printProgress "$cmd"
+    eval "$cmd"
+
     
     popd > /dev/null
 
 }
 
-function clearImage(){
-    imageName="$1"
-    # stop the container
-    docker container stop "$1" 2>/dev/null || true
-    # remove the container
-    docker container rm "$1" 2>/dev/null || true
-    # remove the images
-    result=$(docker images --filter=reference="${imageName}:*" -q | head -n 1)
-    while [ -n "${result}" ]
-    do
-        docker rmi -f ${result}
-        result=$(docker images --filter=reference="${imageName}:*" -q | head -n 1)
-    done
-}
+function deployMultiContainer(){
+    prefix="$1"
+    ContainerRegistryUrl="$2"
+    ContainerRegistryLogin="$3"
+    ContainerRegistryPassword="$4"
+    
+    resourcegroup="${prefix}rg"
+    webapp="${prefix}webapp"
+    # delete containers
+    cmd="az webapp config container delete --name $webapp --resource-group $resourcegroup --output none"
+    printProgress "$cmd"
+    eval "$cmd"
 
+    printProgress "Create Containers"
+    sed "s/{ContainerRegistryUrl}/${ContainerRegistryUrl}\//g"  < $SCRIPTS_DIRECTORY/../src/nginx/docker-compose.template.yml | sed "s/{APP_VERSION}/${APP_VERSION}/g" >  $SCRIPTS_DIRECTORY/../src/nginx/docker-compose-app-service.yml
+    cmd="az webapp config container set --resource-group  "$resourcegroup" --name "$webapp" \
+            --multicontainer-config-type compose --multicontainer-config-file $SCRIPTS_DIRECTORY/../src/nginx/docker-compose-app-service.yml --output none"
+    printProgress "$cmd"
+    eval "$cmd"
+
+    printProgress "Create Config"
+    cmd="az webapp config appsettings set -g "$resourcegroup" -n "$webapp" \
+    --settings DOCKER_REGISTRY_SERVER_URL="$ContainerRegistryUrl" \
+    DOCKER_REGISTRY_SERVER_USERNAME="$ContainerRegistryLogin" \
+    DOCKER_REGISTRY_SERVER_PASSWORD="$ContainerRegistryPassword" --output none"
+    printProgress "$cmd"
+    eval "$cmd"
+
+    printProgress "Restart WebApp"
+    cmd="az webapp restart --name $webapp --resource-group $resourcegroup --output none"
+    printProgress "$cmd"
+    eval "$cmd"
+
+}
 
 # Check Azure connection
 printMessage "Check Azure connection for subscription: '$AZURE_SUBSCRIPTION_ID'"
@@ -123,43 +168,37 @@ checkError
 # Deploy infrastructure image
 printMessage "Deploy infrastructure subscription: '$AZURE_SUBSCRIPTION_ID' region: '$AZURE_REGION' prefix: '$AZURE_APP_PREFIX' sku: 'B2'"
 deployAzureInfrastructure $AZURE_SUBSCRIPTION_ID $AZURE_REGION $AZURE_APP_PREFIX "B2"
-printMessage "Azure Container Registry DNS name: ${ARC_LOGIN_SERVER}"
+printMessage "Azure Container Registry DNS name: ${ACR_LOGIN_SERVER}"
 printMessage "Azure Web App Url: ${WEB_APP_SERVER}"
+printMessage "Azure Container Registry Login: ${ACR_LOGIN}"
+printMessage "Azure Container Registry Password: ${ACR_PASSWORD}"
 
-exit 1
 
 # Build nginx-api docker image
 printMessage "Building nginx-api container"
-clearImage "nginx-api"
-buildWebAppContainer "./src/nginx" "nginx-api" "${APP_VERSION}" "latest" 
+buildWebAppContainer "${ACR_LOGIN_SERVER}" "./src/nginx" "nginx-api" "${APP_VERSION}" "latest" 
 checkError
 
 # Build fastapi-api docker image
 printMessage "Building fastapi-rest-api container version:${APP_VERSION} port: ${APP_PORT}"
-clearImage "fastapi-rest-api"
-buildWebAppContainer "./src/fastapi-rest-api" "fastapi-rest-api" "${APP_VERSION}" "latest" ${APP_PORT}
+buildWebAppContainer "${ACR_LOGIN_SERVER}" "./src/fastapi-rest-api" "fastapi-rest-api" "${APP_VERSION}" "latest" ${APP_PORT}
 checkError
 
 # Build flask-api docker image
 printMessage "Building flask-rest-api container version:${APP_VERSION} port: ${APP_PORT}"
-clearImage "flask-rest-api"
-buildWebAppContainer "./src/flask-rest-api" "flask-rest-api" "${APP_VERSION}" "latest" ${APP_PORT}
+buildWebAppContainer "${ACR_LOGIN_SERVER}" "./src/flask-rest-api" "flask-rest-api" "${APP_VERSION}" "latest" ${APP_PORT}
 checkError
 
-# Deploy nginx, fastapi-rest-api, flask-rest-api
-printMessage "Deploying all the containers"
-targetDirectory="$(dirname "${BASH_SOURCE[0]}")/../src/nginx"
-pushd "$targetDirectory" > /dev/null
-docker-compose up  -d
-checkError
-popd > /dev/null
+printMessage "Deploy containers from Azure Container Registry ${ACR_LOGIN_SERVER}"
+deployMultiContainer "$AZURE_APP_PREFIX" "${ACR_LOGIN_SERVER}" "${ACR_LOGIN}" "${ACR_PASSWORD}"
+
 
 # Test services
 # Test flask-rest-api
 # get nginx local ip address
 # connect to container network if in dev container
 ip=$(get_local_host "nginx-api")
-flask_rest_api_url="http://$ip/flask-rest-api/version"
+flask_rest_api_url="https://${WEB_APP_SERVER}/flask-rest-api/version"
 printMessage "Testing flask-rest-api url: $flask_rest_api_url expected version: ${APP_VERSION}"
 result=$(checkUrl "${flask_rest_api_url}" "${APP_VERSION}" "60")
 if [[ $result != "true" ]]; then
@@ -169,7 +208,7 @@ else
 fi
 
 # Test flask-rest-api
-fastapi_rest_api_url="http://$ip/fastapi-rest-api/version"
+fastapi_rest_api_url="https://${WEB_APP_SERVER}/fastapi-rest-api/version"
 printMessage "Testing fastapi-rest-api url: $fastapi_rest_api_url expected version: ${APP_VERSION}"
 result=$(checkUrl "${fastapi_rest_api_url}" "${APP_VERSION}" "60")
 if [[ $result != "true" ]]; then
@@ -178,12 +217,9 @@ else
     printMessage "Testing fastapi-rest-api successful"
 fi
 
-# Undeploy nginx, fastapi-rest-api, flask-rest-api
-printMessage "Undeploying all the containers"
-targetDirectory="$(dirname "${BASH_SOURCE[0]}")/../src/nginx"
-pushd "$targetDirectory" > /dev/null
-docker-compose down 
-checkError
-popd
+exit 1
+# Undeploy Azure resource 
+printMessage "Undeploying all the Azure resources"
+undeployAzureInfrastructure $AZURE_SUBSCRIPTION_ID $AZURE_APP_PREFIX
 
 echo "done."
